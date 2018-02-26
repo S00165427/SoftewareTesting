ba830924e88727a9986a76a571c98ce69a0818ff
s00165427 <s00165427@mail.itsligo.ie>
2018-02-26 10:40:45 +0000

    tests

---
+++ TestingSoftware.Tests/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace TestingSoftware.Tests
+{
+    [TestFixture]
+
+    class PremiumTests
+    {
+        [Test]
+        public void femaleAgeTest1()
+        {
+            var female = new Class1();
+            Assert.AreEqual(5, female.CalcPremium(20, "female"));
+        }
+
+        [Test]
+        public void femaleAgeTest2()
+        {
+            var female = new Class1();
+            Assert.AreEqual(2.5, female.CalcPremium(34, "female"));
+        }
+
+        [Test]
+        public void femaleAgeTest3()
+        {
+            var female = new Class1();
+            Assert.AreEqual(0, female.CalcPremium(16, "female"));
+        }
+
+        [Test]
+        public void maleAgeTest1()
+        {
+            var male = new Class1();
+            Assert.AreEqual(6, male.CalcPremium(19, "male"));
+        }
+
+        [Test]
+        public void maleAgeTest2()
+        {
+            var male = new Class1();
+            Assert.AreEqual(5, male.CalcPremium(38, "male"));
+        }
+
+        [Test]
+        public void maleAgeTest3()
+        {
+            var male = new Class1();
+            Assert.AreEqual(0, male.CalcPremium(17, "male"));
+        }
+
+        [Test]
+        public void ageTest1()
+        {
+            var age = new Class1();
+            Assert.AreEqual(5 * 0.15, age.CalcPremium(52, "male"));
+        }
+
+        [Test]
+        public void ageTest2()
+        {
+            var age = new Class1();
+            Assert.AreEqual(2.5 * 0.15, age.CalcPremium(54, "female"));
+        }
+
+        [Test]
+        public void GenderTest()
+        {
+            var gender = new Class1();
+            Assert.AreEqual(0, gender.CalcPremium(20, ""));
+        }
+    }
+    
+    
+
+}
---
+++ TestingSoftware.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestingSoftware.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingSoftware.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a45590df-8f15-4c54-bf78-ee1bb59f3a1d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
